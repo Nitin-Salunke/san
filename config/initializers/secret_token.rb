e4fd22eac44d9858a121119d7be8a654fc9c0174
nitin <nitin.salunke@presentsoft.co.in>
2014-07-06 00:14:54 +0530

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sanapp::Application.config.secret_token = '808567bd54281575dbb4c57a8e527661a04295f109b6953cddc558a8c99b0bec0fe7afbe9f061fe52f2ea3a73ca99eb5a887d27dde7e70663a34a513c2cc6475'
